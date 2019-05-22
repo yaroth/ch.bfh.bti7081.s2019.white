3442dfa3b30e8a977a952031b69d39414e0c7097
Nico <nico17dev@gmail.com>
2019-05-22 22:13:43 +0200

    code cleaned

--- src/main/java/despresso/CalendarAction.java
+++
@@ -1,7 +0,0 @@
-package despresso;
-
-public class CalendarAction {
-
-    public final static String CLICK_ME = "init calendar";
-    public final static String RESET = "reset";
-}
--- src/main/java/despresso/logic/CalendarList.java
+++ src/main/java/despresso/logic/CalendarList.java
@@ -2,11 +2,4 @@ package despresso.logic;
 
 public class CalendarList {
 
-    public String getClickMeResult() {
-        return "Button clicked!";
-    }
-
-    public String getResetResult() {
-        return "Test reset!";
-    }
 }
