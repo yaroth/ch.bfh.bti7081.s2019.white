bfe2af66cf13498c952859164c7b7a957cf2e23f
Remo Hofmann <remohofmann@gmx.ch>
2019-05-27 17:52:26 +0200

    add TIP initial DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,17 @@ CREATE TABLE user (
 INSERT INTO user (first_name, last_name, dob) VALUES
   ( 'Yann', 'Roth',  '1974-01-14'),
   ( 'Remo', 'Hofmann', '1987-06-09');
+
+DROP TABLE IF EXISTS tip;
+
+CREATE TABLE tip (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   description VARCHAR(250) NOT NULL,
+   tipDuration VARCHAR(6) NOT NULL,
+   tipLocation VARCHAR(8) NOT NULL,
+   tipType VARCHAR(4) NOT NULL
+);
+
+INSERT INTO tip (description, tipDuration, tipLocation, tipType) VALUES
+    ('Testen', 'LONG', 'ATWORK', 'MIND'),
+    ('Kaffe trinken', 'LONG', 'ATWORK', 'MIND');
