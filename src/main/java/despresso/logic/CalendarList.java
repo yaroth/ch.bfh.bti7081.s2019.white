7966d07ba17a01958c5fa08eb425206ac9a01456
Nico <nico.lieberherr@students.bfh.ch>
2019-06-10 12:12:46 +0200

    started with the calendar repository binding

--- src/main/java/despresso/logic/CalendarList.java
+++ src/main/java/despresso/logic/CalendarList.java
@@ -3,11 +3,11 @@ package despresso.logic;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import despresso.persistence.CalendarRepository;
-import org.springframework.expression.spel.ast.NullLiteral;
 import org.vaadin.stefan.fullcalendar.Entry;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Optional;
 
 
 @UIScope
@@ -32,5 +32,10 @@ public class CalendarList extends ArrayList<CalendarEntry> {
 
     public void deleteCalendarEntry(Entry entry){
         this.stream().filter(m -> m.getId().equals(entry.getId())).findFirst().ifPresent(currentEntry -> _repository.delete(currentEntry));
+    }
+
+    public Optional<CalendarEntry> getNextCalendarEntry(){
+        LocalDateTime now = LocalDateTime.now();
+        return this.stream().filter(m -> m.getStart().isAfter(now) && !m.getIsDone()).findFirst();
     }
 }
