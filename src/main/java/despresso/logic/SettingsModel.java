fc2ec708b3ab93b6dca4b8ac0e5a98cd1ccec941
yr <yannkristen.roth@students.bfh.ch>
2019-06-11 11:59:13 +0200

    removes user repository testing in settingsModel.

--- src/main/java/despresso/logic/SettingsModel.java
+++ src/main/java/despresso/logic/SettingsModel.java
@@ -2,11 +2,6 @@ package despresso.logic;
 
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
-import despresso.DataType;
-import despresso.persistence.UserRepository;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @UIScope
 @SpringComponent
@@ -29,37 +24,7 @@ public class SettingsModel {
 
     public String deleteData() {
         //do the actual changes
-
-        UserRepository userRepository = new UserRepository();
-        // TODO: TESTING purpose *remove these lines* once testing is done ********************************************.
-        User user = userRepository.getByID(1);
-        System.out.println("get by id: " + user);
-
-        User lukas = new User("Lukas", "Zoller", LocalDate.of(1981, 5, 25));
-        userRepository.insert(lukas);
-
-        List<User> userList = userRepository.getAll();
-        System.out.println("Added new user");
-        System.out.println(userList);
-
-        DataTypeInterface newLukas = userRepository.getByID(3);
-        if (newLukas != null) {
-            lukas = ((User) newLukas);
-            lukas.setDob(LocalDate.of(1981, 5, 11));
-            userRepository.update(lukas);
-            userList = userRepository.getAll();
-            System.out.println("Modified user with id=3");
-            System.out.println(userList);
-
-            userRepository.delete(lukas);
-
-            userList = userRepository.getAll();
-            System.out.println("Deleted user with id=3");
-            System.out.println(userList);
-        }
-        // TODO: END of *remove these lines *******************************************************
-
-        return ("Personal h2DBConnector deleted!");
+        return ("Personal data deleted!");
     }
 
     public String deleteAccount() {
